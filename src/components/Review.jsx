85b7f71a0e79617d997c294d589905c8b16bd633
cxmelloo <madinamelibaeva22@gmail.com>
2026-09-15 17:40:40 +0200

    Review edit

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -8,7 +8,7 @@ function Review() {
         <div className="ml-0 md:ml-20 pt-10 md:pt-0 pb-10 md:pb-0 w-full">
           <h1 className="font-bold text-4xl text-[#111827]">Using products?</h1>
           <h2 className="font-semibold text-4xl text-[#6B7280] mb-10">Write a review</h2>
-          <button className="blue-btn w-full md:px-7">Write a review</button>
+          <button className="blue-btn w-full md:w-fit md:px-7">Write a review</button>
         </div>
 
         <div>
